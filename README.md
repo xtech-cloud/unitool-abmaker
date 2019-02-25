5f2246d36e43534fbd3fa7154f451c7270fc6173
Easlee Liu <easlee@outlook.com>
2019-02-25 23:40:20 +0800

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# unitool-abmaker
-Asset Bundle Maker
+# 使用教程
+
+## 打开项目
+
+- 使用Unity打开ABMaker工程。
+
+## 创建资源组文件夹
+
+- 在Assets/Packages目录下创建一个文件夹，用于区别资源分组，例如model.character。
+
+## 创建资源包
+- 右键单机资源组文件夹，例如model.character。
+- 在弹出的菜单中选择"New Asset",会自动生成一个uuid的文件夹，例如d3978ab959294e2c87afe284c6f4612c。
+- 在自动创建的uuid命名的文件夹中，会自动生成一个_manifest.asset文件，此文件用于保存资源包的相关信息，其中只有Alias字段需要手动填写，Alias表示资源包或资源文件的别名，其余字段会自动更新。
+- 将原始素材放到Assets/Source文件夹下， 此处使用Unity官方素材Robot Kyle。
+- 将需要打包到AssetBundle的文件放到uuid形式的资源文件夹中。例如robot.prefab。(Unity打包AssetBundle时会自动处理引用关系，所以其他被引用到的文件可以放在Source目录中不予理会)。例如此处打包prefab文件，prefab用到的fbx、mat、tag等文件依然在Source/Robot Kyle中。
+- 使用菜单栏 BuildTools/Refresh 刷新数据，此操作将填充_manifest.asset文件，并为资源文件夹中的所有除_manifest.asset的其他文件设置AssetBundleName（只有设置了AssetBundleName的文件才会被打包到AssetBundle中）。
+
+## 构建资源包
+- 首先执行BuildTools/Refresh进行一次刷新。
+- 执行BuildTools/Manifest/Export导出资源清单文件，输出目录为和ABMaker文件夹同级的_assets/meta。
+- 执行BuildTools/AssetBundle/Win32构建windows平台的assetbundle，输出目录为和ABMaker同级的_assets/win32。
+
+# 扩展教程
+
+## 直接在Unity素材包中创建资源的方法
+
+- 导入Unity素材包后，将文件夹移到Source目录。
+- 选中需要处理的文件，一般情况下是prefab文件，也可以是音频文件和材质文件或者图片等。
+- 在选中的文件上执行右键菜单中的"Process",完成后会在Assets/_out目录得到很多uuid命名的文件，将这些文件夹移动到对应的资源组文件夹中。
+- 执行BuildTools/Refresh刷新数据。
+
+
---
+++ unitysln/ABMaker/Assets/Editor/BuildTools.cs
@@ -0,0 +1,276 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using SimpleJSON;
+using System.Text.RegularExpressions;
+
+[System.Serializable]
+public class ABFile
+{
+    public string name = "";
+    public string alias = "";
+}
+
+public class ABPack : ScriptableObject
+{
+
+    public string uuid = "";
+
+    public string path = "";
+
+    public string alias = "";
+    public List<ABFile> files = new List<ABFile>(0);
+}
+
+public static class BuildTools
+{
+    //--------------------------------
+    // Settings
+    //--------------------------------
+
+    static string outpath = System.IO.Path.Combine(Application.dataPath, "../../_assets/");
+    static string outpath_manifest = System.IO.Path.Combine(outpath, "meta");
+
+    [MenuItem("BuildTools/AssetBundle/WebGL")]
+    public static void BuildAssetBundleForWebGL()
+    {
+        buildAssetBundle(BuildTarget.WebGL);
+    }
+
+    [MenuItem("BuildTools/AssetBundle/Win32")]
+    public static void BuildAssetBundleForWin32()
+    {
+        buildAssetBundle(BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("BuildTools/AssetBundle/Android")]
+    public static void BuildAssetBundleForAndroid()
+    {
+        buildAssetBundle(BuildTarget.Android);
+    }
+
+    private static void buildAssetBundle(BuildTarget _target)
+    {
+        Directory.CreateDirectory(outpath);
+        string path = System.IO.Path.Combine(outpath, convertToPlatform(_target));
+        Directory.CreateDirectory(path);
+        Debug.Log(string.Format("build assetbundle at {0}", path));
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ForceRebuildAssetBundle, _target);
+
+        //remove unused files
+        File.Delete(Path.Combine(path, convertToPlatform(_target)));
+        File.Delete(Path.Combine(path, convertToPlatform(_target)) + ".manifest");
+        foreach (string file in Directory.GetFiles(path))
+        {
+            if (!file.EndsWith(".manifest"))
+                continue;
+            string target = Path.Combine(path, Path.GetFileName(file));
+            File.Delete(target);
+        }
+    }
+
+    private static string convertToPlatform(BuildTarget _target)
+    {
+        if (BuildTarget.StandaloneWindows == _target)
+            return "win32";
+        if (BuildTarget.StandaloneWindows64 == _target)
+            return "win64";
+        if (BuildTarget.WebGL == _target)
+            return "webgl";
+        if (BuildTarget.Android == _target)
+            return "android";
+        return "unknow";
+    }
+
+
+
+    [MenuItem("BuildTools/Manifests/Export")]
+    public static void BuildManifests()
+    {
+        Directory.CreateDirectory(outpath_manifest);
+        buildManifests();
+    }
+
+    [MenuItem("BuildTools/Refresh")]
+    public static void Refresh()
+    {
+        refresh();
+    }
+
+    private static void refresh()
+    {
+
+        int count = 0;
+
+        string path = System.IO.Path.Combine(Application.dataPath, "Packages");
+
+        DirectoryInfo test = new DirectoryInfo(path);
+        if (test.Exists)
+        {
+            foreach (string group in Directory.GetDirectories(path))
+            {
+                string groupname = Path.GetFileName(group);
+
+                foreach (string package in Directory.GetDirectories(group))
+                {
+                    string packagename = Path.GetFileName(package);
+
+                    List<string> files = new List<string>();
+                    ABPack pack = null;
+                    foreach (string file in Directory.GetFiles(package))
+                    {
+                        string filename = Path.GetFileName(file);
+
+                        string assetPath = string.Format("Assets/Packages/{0}/{1}/{2}", groupname, packagename, filename);
+
+                        //ignore unity's meta file
+                        if (file.EndsWith(".meta"))
+                        {
+                            continue;
+                        }
+
+                        if (Path.GetFileName(file).Equals("_manifest.asset"))
+                        {
+                            pack = AssetDatabase.LoadAssetAtPath<ABPack>(assetPath);
+                            pack.uuid = packagename;
+                            pack.path = groupname;
+                            continue;
+                        }
+
+                        AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant(packagename, "");
+                        files.Add(Path.GetFileName(file));
+                    }
+
+                    if (null == pack)
+                    {
+                        continue;
+                    }
+
+                    Dictionary<string, ABFile> abFiles = new Dictionary<string, ABFile>();
+                    foreach(ABFile abFile in pack.files)
+                    {
+                        abFiles.Add(abFile.name, abFile);
+                    }
+                    pack.files.Clear();
+
+                    foreach (string file in files)
+                    {
+                        ABFile abFile = new ABFile();
+                        abFile.name = Path.GetFileNameWithoutExtension(file);
+                        if(abFiles.ContainsKey(abFile.name))
+                        {
+                            abFile.alias = abFiles[abFile.name].alias;
+                        }
+                        pack.files.Add(abFile);
+                    }
+
+                    count += 1;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Without this directory...");
+        }
+        
+        AssetDatabase.RemoveUnusedAssetBundleNames();
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        Debug.Log("refresh finish");
+    }
+
+    private static void buildManifests()
+    {
+        string path = System.IO.Path.Combine(Application.dataPath, "Packages");
+
+        DirectoryInfo test = new DirectoryInfo(path);
+        if (test.Exists)
+        {
+            foreach (string group in Directory.GetDirectories(path))
+            {
+                string groupname = Path.GetFileName(group);
+                foreach (string package in Directory.GetDirectories(group))
+                {
+                    string packagename = Path.GetFileName(package);
+
+                    foreach (string file in Directory.GetFiles(package))
+                    {
+                        string filename = Path.GetFileName(file);
+                        
+                        if(!Path.GetFileName(file).Equals("_manifest.asset"))
+                            continue;
+
+                        string mfPath = string.Format("Assets/Packages/{0}/{1}/{2}", groupname, packagename, filename);
+                        ABPack pack = AssetDatabase.LoadAssetAtPath<ABPack>(mfPath);
+                        if(null == pack)
+                            continue;
+
+                        string outPath = Path.Combine(outpath_manifest, packagename + ".mf");
+                        string json = JsonUtility.ToJson(pack, true);
+                        File.WriteAllText(outPath, json);
+                        Debug.Log(string.Format("Write {0}.mf to {1}", packagename, outPath));
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Without this directory...");
+        }
+    }
+
+    [MenuItem("Assets/New Asset")]
+    public static void New()
+    {
+        string[] selection = Selection.assetGUIDs;
+        if (selection.Length > 1)
+            return;
+
+        string selectPath = AssetDatabase.GUIDToAssetPath(selection[0]);
+
+        string uuid = System.Guid.NewGuid().ToString().Replace("-", "");
+        string group = selectPath.Replace("Assets/Packages/", "");
+
+        Directory.CreateDirectory(Path.Combine(selectPath, uuid));
+
+        ABPack pack = ScriptableObject.CreateInstance<ABPack>();
+        pack.uuid = uuid;
+        pack.path = group.Replace(".", "/");
+        string mfPath = string.Format("Assets/Packages/{0}/{1}/_manifest.asset", group, uuid);
+        AssetDatabase.CreateAsset(pack, mfPath);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("Assets/Process")]
+    public static void Process()
+    {
+        string[] selections = Selection.assetGUIDs;
+
+        string outpath = System.IO.Path.Combine(Application.dataPath, "_out");
+        Directory.CreateDirectory(outpath);
+
+        foreach(string selection in selections)
+        {
+            string uuid = selection;
+            Directory.CreateDirectory(Path.Combine(outpath, uuid));
+
+            string selectPath = AssetDatabase.GUIDToAssetPath(selection);
+
+            string mfPath = string.Format("Assets/_out/{0}/_manifest.asset", uuid);
+            ABPack pack = ScriptableObject.CreateInstance<ABPack>();
+            pack.uuid = uuid;
+            pack.path = "";
+            AssetDatabase.CreateAsset(pack, mfPath);
+
+            string filePath = string.Format("Assets/_out/{0}/{1}", uuid, Path.GetFileName(selectPath));
+            AssetDatabase.CopyAsset(selectPath, filePath);
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+}
